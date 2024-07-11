f59dd49fe29e1854a0a9e886647a4a9cdb392f0a
hvsigina <harshavsig@gmail.com>
2024-07-11 22:24:26 +0530

    UI Implementation test

--- userInterface.py
+++ userInterface.py
@@ -1,61 +1,69 @@
 import numpy as np
 import gradio as gr
 
-def sepia(image):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189], 
-        [0.349, 0.686, 0.168], 
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = image.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
 
+image_count = 0
 
-def test(name, intensity):
-    return "HELLO " + name +"!" * int(intensity)
+def change_tab(i):
+    return gr.Tabs(selected=i)
 
-#text inputs
-demo = gr.Interface(
-    fn = test,
-    inputs = ["text",gr.Slider(value=4, minimum=2, maximum=8, step=2)],
-    outputs=[gr.Textbox(label="GREETINGS", lines=3)],
-)
+def goto_step2():
+    return change_tab(2)
 
-#image inputs
-imgDemo = gr.Interface(
-    fn = sepia,
-    inputs = [gr.Image()],
-    outputs= [gr.Image()],
-)
+def goto_step3():
+    return change_tab(3)
 
-#text inputs
-def calculator(num1, operation, num2):
-    if operation == "add":
-        return num1 + num2
-    elif operation == "subtract":
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == "divide":
-        if num2 == 0:
-            raise gr.Error("Cannot divide by zero!")
-        return num1 / num2
+def goto_step4():
+    return change_tab(4)
+
+def galFunc():
+    global image_count
+    image_count = image_count+1
+    return
+
+demo = gr.Blocks()
+
+with demo:
+    gr.Markdown(
+        """
+    # 3DMFFR
+    3D Face Reconstruction from one or Multiple 2D images, using cuda-accelerated renderer.
+    """
+    )
+    with gr.Tabs() as tabs:
+        with gr.TabItem("Step 1: Faceset extraction", id=1 ):
+            with gr.Row():
+                text_input1 = gr.Textbox(placeholder="dummy")
+                text_output1 = gr.Textbox(placeholder="dummy")
+                text_button1 = gr.Button("dumbo")
+                
+            with gr.Row():
+                text_input2 = gr.Textbox(placeholder="dummy")
+                text_output2 = gr.Textbox(placeholder="dummy")
+            text_button2 = gr.Button("CONTINUE TO STEP 2")
+        with gr.TabItem("Step 2: Landmarks creation and options selection", id=2):
+            with gr.Row():
+                image_input3 = gr.Image()
+                image_output3 = gr.Image()
+                image_button3 = gr.Button("Flip")
+            with gr.Row():
+                radio1 = gr.Radio(choices=["Jaffa","Guffa","Laffa","Ruffa"])
+            radio_button = gr.Button("CONINUE TO STEP 3")
+        with gr.TabItem("Step 3: Training", id=3):
+            with gr.Row():
+                image_input9 = gr.Image()
+                gallery = gr.Gallery()
+            final_button = gr.Button("CONTINUE TO STEP 4")
+        with gr.TabItem("Step 4: Object Creation", id=4):
+            textoutputfinal = gr.Textbox(value=image_count)
+            
+            text_button2.click()
+    text_button2.click(goto_step2)
     
-calcDemo = gr.Interface(
-    calculator,
-    [
-        "number",
-        gr.Radio(["add","subtract","multiply","divide"]),
-        "number"
-    ],
-    "number",
-    examples=[
-        [45,"add",89],
-    ],
-    title = "CALC",
-    description="Here's a sample toy calculator."
-)
-
-calcDemo.launch()
-
+    radio_button.click(goto_step3)
+    
+    final_button.click(goto_step4)
+    
+    gallery.upload(galFunc)
+    
+demo.launch()
